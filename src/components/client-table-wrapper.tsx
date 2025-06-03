04cf23ca02b8f82cd0602a68ec63e182846838d7
Brian Best <contact@brian.best>
2025-06-03 11:40:04 -0400

    bugfix: hydration error DND Kit

--- src/components/client-table-wrapper.tsx
+++ src/components/client-table-wrapper.tsx
@@ -4,7 +4,6 @@ import { ColumnDef } from "@tanstack/react-table";
 import dynamic from "next/dynamic";
 import { User } from "../types/user";
 
-// Dynamically import the UserTable component with SSR disabled
 const UserTable = dynamic(
   () => import("./user-table").then((mod) => mod.UserTable),
   { ssr: false }
